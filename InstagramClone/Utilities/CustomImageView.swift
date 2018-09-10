2ba6788f19320c887f02d37d3ca22b83224361d9
Kangyi Wang <164071754@qq.com>
2018-09-10 14:11:55 +1000

    Initial

---
+++ InstagramClone/Utilities/CustomImageView.swift
@@ -0,0 +1,44 @@
+//
+//  CustomImageView.swift
+//  InstagramClone
+//
+//  Created by Mac Gallagher on 7/28/18.
+//  Copyright © 2018 Mac Gallagher. All rights reserved.
+//
+
+import UIKit
+
+var imageCache = [String: UIImage]()
+
+class CustomImageView: UIImageView {
+    
+    private var lastURLUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        lastURLUsedToLoadImage = urlString
+        image = nil
+        
+        if let cachedImage = imageCache[urlString] {
+            image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            
+            if url.absoluteString != self.lastURLUsedToLoadImage { return }
+            
+            guard let imageData = data else { return }
+            let photoImage = UIImage(data: imageData)
+            imageCache[url.absoluteString] = photoImage
+            
+            DispatchQueue.main.async {
+                self.image = photoImage
+            }
+        }.resume()
+    }
+}
